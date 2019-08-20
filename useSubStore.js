6d10c74b54fd772bc5bd6f6ccdd6db81033c9296
antonio <antonio@pykiss.com>
2019-08-20 08:33:18 +0200

    sync

---
+++ useSubStore.js
@@ -0,0 +1,29 @@
+const { useEffect } = require('react')
+const store = require('../reduxplus/src/store')
+
+module.exports = function useSubStore (options) {
+  const {
+    key,
+    hydrate,
+    replace,
+    clean,
+    reducers
+  } = options
+
+  const sub = store.subStore(key)
+
+  if (hydrate) {
+    sub.hydrate(hydrate, replace)
+  }
+
+  if (reducers && reducers.length) {
+    reducers.forEach(r => {
+      sub.setReducer(r)
+    })
+  }
+
+  useEffect(() => {
+    return sub.cleanDebounced(500, clean)
+  })
+  return sub
+}
